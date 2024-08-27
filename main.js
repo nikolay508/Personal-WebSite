bfc5bb904cf9b87910b0f61e81ae17b93cb8549c
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-08-27 13:05:23 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+import page from './node_modules/page/page.mjs';
+import { showAboutView } from './views/aboutView.js';
+import { showHomeView } from './views/homeView.js';
+import { showServicesView } from './views/servicesView.js';
+import { showWorkView } from './views/workView.js';
+
+page('/', showHomeView);
+page('/about', showAboutView);
+page('/services', showServicesView);
+page('/work', showWorkView);
+page.start();
